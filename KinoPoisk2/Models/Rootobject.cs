9bfea10debdce6d4a043f37fa20712197820d11d
Дмитрий <ahisharit@gmail.com>
2023-01-17 21:55:48 +0700

    170123

--- KinoPoisk2/Models/Rootobject.cs
+++ KinoPoisk2/Models/Rootobject.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 namespace KinoPoisk2.Models;
 
 public class Rootobject
-{
-    public string? status { get; set; }
-    public string? copyright { get; set; }
-    public bool? has_more { get; set; }
-    public int? num_results { get; set; }
-    public Result[]? results { get; set; }
-}
+ {
+     public string? status { get; set; }
+     public string? copyright { get; set; }
+     public bool? has_more { get; set; }
+     public int? num_results { get; set; }
+     public Result[]? results { get; set; }
+ }
